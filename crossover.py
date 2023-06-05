f6d6ac7e6e1ed3265c031d903c4b5fd40d29ec6e
lshadoyan <lshadoyan@gmail.com>
2023-06-04 23:44:07 -0400

    Added profit column

--- crossover.py
+++ crossover.py
@@ -1,18 +1,42 @@
 import pandas as pd
 import numpy as np
+from IPython.display import display
 
 crypto_data = pd.read_csv("bitcoin_data.csv")
-
-crypto_data["Fast_Average"] = crypto_data["Close"].rolling(50).mean()
-crypto_data["Slow_Average"] = crypto_data["Close"].rolling(200).mean()
+crypto_data["Fast_Average"] = crypto_data["Close"].rolling(25).mean()
+crypto_data["Slow_Average"] = crypto_data["Close"].rolling(100).mean()
 crypto_data["Prev_Fast"] = crypto_data["Fast_Average"].shift(1)
+crypto_data["Prev_Slow"] = crypto_data["Slow_Average"].shift(1)
 crypto_data.dropna(inplace=True)
 
 def crossover_finder(prev_fast, fast, slow):
     if((fast > slow) and (prev_fast < slow)):
         return "Bull"
-    elif((fast > slow) and (prev_fast > slow)):
+    elif((fast < slow) and (prev_fast > slow)):
         return "Bear"
-    return None
+    return "Neither"
 crypto_data["Crossover"] = np.vectorize(crossover_finder)(crypto_data["Prev_Fast"], crypto_data["Fast_Average"], crypto_data["Slow_Average"])
 
+profit_column = []
+
+for index_bull in range(len(crypto_data)):
+    if crypto_data.iloc[index_bull]["Crossover"] == "Bull":
+        index_bear = index_bull + 1
+        while index_bear < len(crypto_data) and crypto_data.iloc[index_bear]["Crossover"] != "Bear":
+            index_bear += 1
+        if index_bear < len(crypto_data):
+            profit = crypto_data.iloc[index_bear]["Close"] - crypto_data.iloc[index_bull]["Close"]
+            
+            if profit < 0:
+                profit_column.append("Decreasing")
+            else:
+                profit_column.append("Increasing")
+        else:
+            profit_column.append("")
+    else:
+        profit_column.append("")
+
+
+crypto_data["Profit"] = profit_column
+
+        
