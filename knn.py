8fee7edea5b9d91cdef1e4fbdb951d6a8c0b333e
lshadoyan <lshadoyan@gmail.com>
2023-07-22 17:14:20 -0400

    Added predict functions that don't use sklearn

--- knn.py
+++ knn.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import numpy as np
-from sklearn.model_selection import train_test_split
+import utility
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
+from collections import Counter
+from tqdm import tqdm
 
 class KNN:
-    def __init__(self, filename):
+    def __init__(self, filename, k):
             self.crypto_data = pd.read_csv(filename)
+            self.k = k
 
     def preprocess(self):
         exclude_columns = ['Timestamp', "Exit Price", "Profit/Loss", "Profit Values", "Bear Index", "Profit", 'Taker buy base asset volume', 'Taker buy quote asset volume', 'Exit Price', 'Unnamed: 0', 'Start Price', 'Middle Price', 'End Price']
         numeric_columns = [column for column in self.crypto_data.columns if column not in exclude_columns and self.crypto_data[column].dtype != 'object']
         
-        X = self.crypto_data[numeric_columns].copy()
-        y = self.crypto_data["Profit Indicator"]
+        self.X = self.crypto_data[numeric_columns].copy()
+        self.y = self.crypto_data["Profit Indicator"]
+        self.X = self.scale_data(self.X)
 
-        X = self.scale_data(X)
-        # print(X.shape)
 
-        X_train, X_test, y_train, y_test = self.split_data(X, y, test_size=0.2, random_state=42)
-
+        X_train, X_test, y_train, y_test = self.split_data(self.X, self.y, test_size=0.2, random_state=12)
         return X_train, X_test, y_train, y_test
     
     def scale_data(self, X):
@@ -41,21 +42,36 @@ class KNN:
 
         X_train = X.iloc[train_indices]
         X_test = X.iloc[test_indices]
-        print(X_train.shape)
         y_train = y.iloc[train_indices]
         y_test = y.iloc[test_indices]
 
         return X_train, X_test, y_train, y_test
     
+    def predict(self, X_test, X_train):
+         predict = [self._predict(x, X_train) for x in tqdm(X_test.itertuples(index=False), total=len(X_test))]
+         return predict
+
+    def _predict(self, x, X_train):
+         distances = [utility.euclidean_distance(x, x_train) for index, x_train in X_train.iterrows()]
+         k_near = np.argsort(distances)[:self.k]
+         y_label = [self.y[i] for i in k_near]
+         y_common = Counter(y_label).most_common(1)
+         return y_common[0][0]
+    
     def model_train(self, X_train, y_train):
 
-        knn = KNeighborsClassifier(n_neighbors=30)
+        knn = KNeighborsClassifier(n_neighbors=45)
 
         knn.fit(X_train, y_train)
 
         return knn
 
-    def evaluate(self, model, X_test, y_test):
+    def evaluate(self, y_pred, y_test):
+
+        accuracy = accuracy_score(y_test, y_pred)
+        print("Accuracy:", accuracy)
+    
+    def evaluate2(self, model, X_test, y_test):
         y_pred = model.predict(X_test)
         print(y_pred)
 
